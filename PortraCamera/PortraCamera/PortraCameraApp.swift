cbbfed1ff862e7cb6bfbcb3dd173dfbeb1ba2938
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2025-06-08 21:27:47 -0700

    Initial Xcode project setup

---
+++ PortraCamera/PortraCamera/PortraCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortraCameraApp.swift
+//  PortraCamera
+//
+//  Created by Piyush Hole on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PortraCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
